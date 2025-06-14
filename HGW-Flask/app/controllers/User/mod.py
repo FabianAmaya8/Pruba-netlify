21871ed7df6b28609746e9a723276de12a878e13
Fabian_Amaya <123851947+FabianAmaya8@users.noreply.github.com>
2025-06-14 01:39:29 -0500

    creando un /

--- HGW-Flask/app/controllers/User/mod.py
+++ HGW-Flask/app/controllers/User/mod.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
 mod_bp = Blueprint('mod', __name__)
 
 @mod_bp.route("/api/moderador", methods=["GET"])
 def dashboard():
     return "Moderador"
+
+@mod_bp.route("/", methods=["GET"])
+def index():
+
+    return render_template("index.html")
