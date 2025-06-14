3ffd2b83d2c54c3512898b1feb251277a659bf3f
Fabian_Amaya <123851947+FabianAmaya8@users.noreply.github.com>
2025-06-14 00:21:29 -0500

    actualización

--- HGW-Flask/config.py
+++ HGW-Flask/config.py
@@ -1,6 +1,6 @@
 class Config:
     SECRET_KEY = 'CLAVE'
-    MYSQL_HOST = 'pc-fabian-mysql.at.remote.it:33000'
+    MYSQL_HOST = 'pc-fabian-mysql.at.remote.it'
     MYSQL_USER = 'HGW'
     MYSQL_PASSWORD = 'HGW123'
     MYSQL_DB = 'HGW_database'
